e6874b2be0bb1ba428253bc4d6c4c95eec046db4
Unknown <konata016@gmail.com>
2019-07-05 03:03:13 +0900

    更新

---
+++ SoundTest/Assets/BeatUi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatUi : MonoBehaviour
+{
+
+    public GameObject notes;
+    public GameObject notes1;
+    public GameObject canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Music.IsPlaying && Music.IsJustChangedBeat())
+        {
+            GameObject prefab = Instantiate(notes) as GameObject;
+            prefab.transform.SetParent(canvas.transform, false);
+
+            GameObject prefab1 = Instantiate(notes1) as GameObject;
+            prefab1.transform.SetParent(canvas.transform, false);
+        }
+    }
+}
