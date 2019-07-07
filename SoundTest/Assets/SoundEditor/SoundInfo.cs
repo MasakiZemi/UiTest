ed09081d5e338e05f1c1d06f833fdf3a66d0f6b2
Unknown <konata016@gmail.com>
2019-07-08 03:51:41 +0900

    修正

--- SoundTest/Assets/SoundEditor/SoundInfo.cs
+++ SoundTest/Assets/SoundEditor/SoundInfo.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 using System.IO;
 using System;
 
-
-
 public class SoundInfo : MonoBehaviour
 {
     AudioSource source;
@@ -17,8 +15,7 @@ public class SoundInfo : MonoBehaviour
     public Slider timeSlider;
     bool onMusic = true;
 
-    public List<string> musicScore = new List<string>();
-    public List<float> musicScore_ = new List<float>();
+    public List<float> musicScore = new List<float>();
 
     // Start is called before the first frame update
     void Start()
@@ -29,21 +26,23 @@ public class SoundInfo : MonoBehaviour
         //再生バーの終了位置セット
         timeSlider.maxValue = clip.length;
 
-        //str = File.ReadAllLines("aaa.txt");
 
-        //キャスト
-        musicScore_.AddRange(Array.ConvertAll<string, float>(File.ReadAllLines("aaa.txt"), delegate (string value)
-        {
-            return float.Parse(value);
-        }));
-        musicScore.AddRange(File.ReadAllLines("aaa.txt"));
+
+        //キャスト(.txtをstring型の配列で受け取り、string型の配列をfloat型の配列に直し、float型の配列をfloat型のListに直す)
+        musicScore.AddRange(Array.ConvertAll(File.ReadAllLines("aaa.txt"), float.Parse));
+
+        //str = File.ReadAllLines("aaa.txt");
+        //musicScore_.AddRange(Array.ConvertAll<string, float>(File.ReadAllLines("aaa.txt"), delegate (string value)
+        //{
+        //    return float.Parse(value);
+        //}));
+        //musicScore.AddRange(File.ReadAllLines("aaa.txt"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        //0の排除とソート
-        musicScore.Remove("0");
+        //ソート
         musicScore.Sort();
 
         //BGMの再生時間と再生バーをリンク
@@ -66,17 +65,24 @@ public class SoundInfo : MonoBehaviour
         //Rを押したら時間を記録する
         if (Input.GetKeyDown(KeyCode.R))
         {
-            musicScore.Add(source.time.ToString());
+            musicScore.Add(source.time);
         }
 
         //Sを押したら保存する
         if (Input.GetKeyDown(KeyCode.S))
         {
-            File.WriteAllLines("aaa.txt", musicScore);
+            //キャスト　(float型のListをstring型の入るつに変換)
+            string[] str = Array.ConvertAll(musicScore.ToArray(), delegate (float value)
+             {
+                 return value.ToString();
+             });
+
+            //テキストに書き出し
+            File.WriteAllLines("aaa.txt", str);
         }
 
         //消す
-        if (musicScore.Contains("0")) musicScore.Remove("0");
+        if (musicScore.Contains(0)) musicScore.Remove(0);
 
     }
 }
