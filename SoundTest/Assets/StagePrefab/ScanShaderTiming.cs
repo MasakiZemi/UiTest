74483b9229c3d9c6e578450934d0576541da10c1
d <d>
2019-10-11 20:40:18 +0900

    光の棒が出るぞ！
    時間がずれたら調性するスクリプトを書く

---
+++ SoundTest/Assets/StagePrefab/ScanShaderTiming.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanShaderTiming : MonoBehaviour
+{
+
+    public Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Music.IsPlaying)
+        {
+            material.SetFloat("Vector1_156873A5", 1f);
+
+
+            //光の棒が出るまでの時間を調整したい（1小節ごとに）
+            //現在はできていない
+            material.SetFloat("Vector1_C34C010D", 2f);
+        }
+        //Debug.Log(material.GetInt("MusicTiming"));
+    }
+}
