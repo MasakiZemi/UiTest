dc48e5d352d00f7a915d782fd21942991edc2a96
d <d>
2019-11-06 21:39:12 +0900

    更新

--- SoundTest/Assets/SoundEditor2/Attack/InstantNotes.cs
+++ SoundTest/Assets/SoundEditor2/Attack/InstantNotes.cs
@@ -8,25 +8,91 @@ public class InstantNotes : MonoBehaviour
 {
     public class EnemyAttackTime
     {
-        public bool[] lane = new bool[6];
+        public bool[] lane = new bool[6];           //
         public SoundEditor.ATTACKTYPE attackType;   //敵の攻撃の種類
-        public float musicScore;        //攻撃を出す時間
+        public float musicScore;                    //攻撃を出す時間
     }
     public List<EnemyAttackTime> timeList = new List<EnemyAttackTime>();
 
-    public float speed = 10;
-    public GameObject[] objPos = new GameObject[6];
-    public GameObject obj;
-    public GameObject destroyPos;
+    public float speed = 10;                        //ノーツ速度
+    public GameObject[] objPos = new GameObject[6]; //ノーツ生成場所
+    public GameObject obj;                          //生成元のオブジェクト
+    public GameObject destroyPos;                   //壊すポジション
+    //public GameObject linePos;
     public Slider slider;
+    public Material throwMaterial;
 
-    int listCount;
-    float timer;
-    List<GameObject> notesList = new List<GameObject>();
+    int listCount;                                          //リストの中身を回すよう
+    float timer;                                            //経過時間
+    List<GameObject> notesList = new List<GameObject>();    //ノーツオブジェクトの管理用
+    float fixTime;                                          //修正する時間
 
     // Start is called before the first frame update
     void Start()
     {
+        //ラインに触れるタイミングが登録された時間になるようにするための計算
+        fixTime = objPos[0].transform.position.z / speed;
+
+        OnStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //シークバーの位置と連動させる
+        timer = slider.value;//Time.deltaTime;
+
+        //生成
+        if (timer > timeList[listCount].musicScore - fixTime)
+        {
+            for (int i = 0; i < timeList[listCount].lane.Length; i++)
+            {
+                if (timeList[listCount].lane[i])
+                {
+                    notesList.Add(Instantiate(obj, objPos[i].transform));
+
+                    //スローの時は色を変える
+                    if (timeList[listCount].attackType == SoundEditor.ATTACKTYPE.Throw)
+                    {
+                        notesList[notesList.Count - 1].GetComponent<Renderer>().material = throwMaterial;
+                    }
+                }
+            }
+            listCount++;
+        }
+
+        //初期化
+        if (Input.GetKeyDown(KeyCode.Space)) OnStart();
+
+        Move();
+    }
+    void Move()
+    {
+        for(int i = 0; i < notesList.Count; i++)
+        {
+            if (notesList[i].transform.position.z < destroyPos.transform.position.z)
+            {
+                //削除
+                Destroy(notesList[i]);
+                notesList.RemoveAt(i);
+                if (notesList.Count > i) break;
+            }
+            else
+            {
+                //動き
+                Vector3 pos = notesList[i].transform.position;
+                pos.z -= speed * Time.deltaTime;
+                notesList[i].transform.position = pos;
+            }
+        }
+    }
+
+    void OnStart()
+    {
+        timeList.Clear();
+        listCount = 0;
+
+        //テキストの読み込み
         foreach (string str in File.ReadLines("aaa.txt"))
         {
             timeList.Add(new EnemyAttackTime());
@@ -48,45 +114,6 @@ public class InstantNotes : MonoBehaviour
 
             listCount++;
         }
-
         listCount = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        timer = slider.value;//Time.deltaTime;
-
-        if (timer > timeList[listCount].musicScore)
-        {
-            for (int i = 0; i < timeList[listCount].lane.Length; i++)
-            {
-                if (timeList[listCount].lane[i])
-                {
-                    notesList.Add(Instantiate(obj, objPos[i].transform));
-                }
-            }
-            listCount++;
-        }
-
-        Move();
-    }
-    void Move()
-    {
-        for(int i = 0; i < notesList.Count; i++)
-        {
-            if (notesList[i].transform.position.z < destroyPos.transform.position.z)
-            {
-                Destroy(notesList[i]);
-                notesList.RemoveAt(i);
-                if (notesList.Count < i) break;
-            }
-            else
-            {
-                Vector3 pos = notesList[i].transform.position;
-                pos.z -= speed * Time.deltaTime;
-                notesList[i].transform.position = pos;
-            }
-        }
     }
 }
