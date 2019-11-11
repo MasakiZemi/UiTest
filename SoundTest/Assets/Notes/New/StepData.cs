ec15b6d6b79ffac2081ee1bbe79edc774177c659
d <d>
2019-11-11 18:19:42 +0900

    ノーツタイミング道理に出ない
    バグあり

---
+++ SoundTest/Assets/Notes/New/StepData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class StepData : MonoBehaviour
+{
+    public string scoreName = "aaa";
+    string fileName = "Assets/SoundEditor2/Score/";
+
+    public AudioSource source;     //サウンド
+
+    public enum INPUT_TEXT { MusicScore, PlStep = 8 }
+    public enum PL_STEP_TIMING
+    {
+        Nothing,
+        Step,
+    }
+
+    public class Data
+    {
+        public PL_STEP_TIMING plStep;
+        public float musicScore;
+    }
+    public List<Data> stepData = new List<Data>();
+
+    static StepData StepData_ = new StepData();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int count = 0;
+        stepData.Clear();
+        fileName += scoreName + ".txt";
+
+        //テキストの読み込み
+        foreach (string str in File.ReadLines(fileName))
+        {
+            string[] arr = str.Split(',');                           //（,）カンマで分ける
+            stepData.Add(new Data());
+
+            stepData[count].musicScore = float.Parse(arr[(int)INPUT_TEXT.MusicScore]);
+            stepData[count].plStep = (PL_STEP_TIMING)int.Parse(arr[(int)INPUT_TEXT.PlStep]);
+
+            count++;
+        }
+
+        StepData_.stepData = new List<Data>(stepData);
+        StepData_.source = source;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StepData_.source = source;
+    }
+
+    public static List<Data> GetStepData { get { return StepData_.stepData; } }
+    public static float GetSoundPlayTime { get { return StepData_.source.time;} }
+}
