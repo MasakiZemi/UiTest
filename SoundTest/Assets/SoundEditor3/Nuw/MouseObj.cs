7ea685ce9b59dcb347e88a8426e9b5fe3345bd98
konata016 <konata016@gmail.com>
2019-11-23 02:07:29 +0900

    更新

--- SoundTest/Assets/SoundEditor3/Nuw/MouseObj.cs
+++ SoundTest/Assets/SoundEditor3/Nuw/MouseObj.cs
@@ -12,6 +12,7 @@ public class MouseObj : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //マウスに
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 10;
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
@@ -24,36 +25,65 @@ public class MouseObj : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0)) //マウスがクリックされたら
         {
-            GameObject obj;
+            //触っているオブジェクトのタグによって処理が変わる
             switch (other.gameObject.tag)
             {
                 case "True":
-                    obj = GetObj(other.gameObject, 0);
-                    other.gameObject.SetActive(false);
-                    obj.SetActive(true);
+                    ObjChange(GetFrgObj(other.gameObject, 0));
                     break;
 
                 case "False":
-                    obj = GetObj(other.gameObject, 1);
-                    other.gameObject.SetActive(false);
-                    obj.SetActive(true);
+                    ObjChange(GetFrgObj(other.gameObject, 1));
                     break;
 
                 case "AttNothing":
-
-
+                    ObjChange(GetModeObj(other.gameObject, 1));
                     break;
+
+                case "AttWave":
+                    ObjChange(GetModeObj(other.gameObject, 2));
+                    break;
+
+                case "AttThrow":
+                    ObjChange(GetModeObj(other.gameObject, 0));
+                    break;
+
+                case "PlNothing":
+                    ObjChange(GetModeObj(other.gameObject, 1));
+                    break;
+
+                case "PlNormalStep":
+                    ObjChange(GetModeObj(other.gameObject, 0));
+                    break;
+
                 default: break;
             }
         }
+
+        //表示、非表示の切り替え用のやつ
+        void ObjChange(GameObject changeObj)
+        {
+            other.gameObject.SetActive(false);
+            changeObj.SetActive(true);
+        }
     }
 
-    GameObject GetObj(GameObject obj, int childNum)
+    //フラグ用のオブジェクト取得
+    GameObject GetFrgObj(GameObject obj, int childNum)
     {
 
         int num = (int)obj.transform.position.x;
         GameObject parentObj = obj.transform.parent.gameObject.transform.parent.gameObject;
         GameObject childObj = parentObj.transform.GetChild(childNum).gameObject.transform.GetChild(num).gameObject;
+
+        return childObj;
+    }
+
+    //モード切替用のオブジェクト取得用
+    GameObject GetModeObj(GameObject obj,int num)
+    {
+        GameObject parentObj = obj.transform.parent.gameObject;
+        GameObject childObj = parentObj.transform.GetChild(num).gameObject;
         Debug.Log(parentObj.name);
 
         return childObj;
