51e538f706ef25d3291b8f8b0c16c5545d15618b
Unknown <konata016@gmail.com>
2019-11-29 03:23:49 +0900

    下地だけ作った

---
+++ SoundTest/Assets/PlGroundNotes/GroundBloc.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundBloc : MonoBehaviour
+{
+    public GameObject obj;
+    public float space = 0.2f;
+    public int count = 10;
+    List<GameObject> objList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float sizZ = obj.transform.localScale.z;
+        for(int i = 0; i < count; i++)
+        {
+            objList.Add(Instantiate(obj, new Vector3(0, 0, (space+ sizZ) * i), new Quaternion()));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SoundTest/Assets/PlGroundNotes/LineMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineMove : MonoBehaviour
+{
+    public float speed = 10;
+    public GameObject obj;
+    List<GameObject> objList = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            objList.Add(Instantiate(obj, transform));
+        }
+
+        for(int i = 0; i < objList.Count; i++)
+        {
+            Vector3 pos = objList[i].transform.position;
+            pos.z += speed * Time.deltaTime;
+            objList[i].transform.position = pos;
+        }
+    }
+}
