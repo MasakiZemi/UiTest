00d0be588e398e0c1f2e55f2ceefe457a7208505
d <d>
2019-12-09 22:19:49 +0900

    更新

--- SoundTest/Assets/PlDamageEffect/DissolveControl.cs
+++ SoundTest/Assets/PlDamageEffect/DissolveControl.cs
@@ -6,7 +6,8 @@ public class DissolveControl : MonoBehaviour
 {
     public Material material;
     public float speed = 0.2f;
-    public bool onStart;
+    public bool onFadeOut;
+    public bool onFadeIn;
     float gage = 2;
     // Start is called before the first frame update
     void Start()
@@ -17,11 +18,18 @@ public class DissolveControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (onStart)
+        if (onFadeOut)
         {
             if (0 < gage)
             {
                 material.SetFloat("_Gauge", gage -= Time.deltaTime * speed);
+            }
+        }
+        else if (onFadeIn)
+        {
+            if (2 > gage)
+            {
+                material.SetFloat("_Gauge", gage += Time.deltaTime * speed);
             }
         }
     }
